b509c65e62549e7b95362fb91278827c37c1cbd2
felipeluc <felipelucaspereira246@gmail.com>
2025-07-03 17:07:50 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-lite.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO.firebaseapp.com",
-  projectId: "SEU_PROJETO_ID",
-  storageBucket: "SEU_BUCKET.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyA8RN8vcrLZGGmwCXx8ng4GaUZDSo_SSfg",
+  authDomain: "reunioes-sistema.firebaseapp.com",
+  projectId: "reunioes-sistema",
+  storageBucket: "reunioes-sistema.firebasestorage.app",
+  messagingSenderId: "591533232683",
+  appId: "1:591533232683:web:a2aaeddac1d6c4e3a7906e"
 };
 
 const app = initializeApp(firebaseConfig);
