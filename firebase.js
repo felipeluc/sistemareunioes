049611a929a1428a7ef1a5dbf46e0b44c3103e08
felipeluc <felipelucaspereira246@gmail.com>
2025-07-03 10:39:16 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8RN8vcrLZGGmwCXx8ng4GaUZDSo_SSfg",
+  authDomain: "reunioes-sistema.firebaseapp.com",
+  projectId: "reunioes-sistema",
+  storageBucket: "reunioes-sistema.firebasestorage.app",
+  messagingSenderId: "591533232683",
+  appId: "1:591533232683:web:a2aaeddac1d6c4e3a7906e"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
