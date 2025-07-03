fd0ff31e08a70b7a50dd52b183ef9200841d724f
felipeluc <felipelucaspereira246@gmail.com>
2025-07-03 17:18:16 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA8RN8vcrLZGGmwCXx8ng4GaUZDSo_SSfg",
   authDomain: "reunioes-sistema.firebaseapp.com",
   projectId: "reunioes-sistema",
-  storageBucket: "reunioes-sistema.firebasestorage.app",
+  storageBucket: "reunioes-sistema.appspot.com",
   messagingSenderId: "591533232683",
   appId: "1:591533232683:web:a2aaeddac1d6c4e3a7906e"
 };
